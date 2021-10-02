1287b9c270f3d6ef25061113ac96f0b24da5e8f2
baird11 <46852525+baird11@users.noreply.github.com>
2021-10-01 23:01:37 -0700

    Added main activity comments

--- app/src/main/java/com/example/cs301_hw2/MainActivity.java
+++ app/src/main/java/com/example/cs301_hw2/MainActivity.java
@@ -1,3 +1,17 @@
+/**Author: Bruce Baird
+ * Date: Oct. 1, 2021
+ * Project: Homework #2, 15 Squares puzzle
+ *
+ * Description: code creates a game where you try to solve th e15 squares puzzle by
+ * moving tiles around into the correct order by only exchanging tiles neigboring the empty tile
+ * with said empty tile
+ *
+ * Extensions:
+ * 1.  you drag the empty tile onto an adjacent tile instead of using two clicks
+ * 2.  the seekbar on the side allows you to change teh size of the puzzle from 2X2 to 7X7
+ *
+ */
+
 package com.example.cs301_hw2;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +27,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Give objects on screen variable names
         SeekBar numberSquaresBar = (SeekBar) findViewById(R.id.numberSquaresBar);
         Button resetButton = (Button) findViewById(R.id.resetButton);
         BoardSurfaceView boardView = (BoardSurfaceView) findViewById(R.id.boardSurfaceView);
 
+        //set listeners for the different elements
         boardView.setOnTouchListener(boardView);
         resetButton.setOnClickListener(boardView);
         numberSquaresBar.setOnSeekBarChangeListener(boardView);
